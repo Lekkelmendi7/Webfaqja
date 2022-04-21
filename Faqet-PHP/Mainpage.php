8864c456581dc8fd706d44eef3db3abcf02d95bb
Lekkelmendi7 <95182111+Lekkelmendi7@users.noreply.github.com>
2022-04-21 23:46:14 +0300

    Add files via upload

---
+++ Faqet-PHP/Mainpage.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Faqja Kryesore</title>
+        <link rel="stylesheet" type="text/css" href="mainpage.css">
+
+        <link rel="stylesheet" href="css/footer.css">
+        <link rel="stylesheet" href="css/navbar.css">
+
+       
+    </head>
+    <body>
+        <header>
+        <?php
+        include_once "C:\xampp\htdocs\Webfaqja\includes\footer.php";
+        ?>
+        </header>
+
+        
+        <main>
+            <div class="main">
+              <div class="banner">
+                <img src="photos\comp.jpg"><a href="Mainpage.html"></a>
+                <div class="first_txt">Miresevini ne webfaqen tone!</div>
+              </div>
+              <div class="pic-box">
+                <hr>
+                <h3>Kategorite</h3>
+
+                <div class="images">
+                    <div class="four">
+                        <div class="four-img">
+                            <div class="img-square">
+                                <img src="photos\kompjuter.jpg" class="" ><a href="#"></a>
+                                <div class="img-info">KOMPJUTERE</div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\laptops.jpg" ><a href="#"></a>
+                                <div class="img-info">
+                                  LAPTOP
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\pjese_harduerike.jpg" >
+                                <div class="img-info">
+                                  PJESE<br>HARDUERIKE
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\aksesore.jpg" >
+                                <div class="img-info">
+                                  AKSESORE
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="four">
+                        <div class="four-img">
+                            <div class="img-square">
+                                <img src="photos\aparate_fotografike.jpg" class="" >
+                                <div class="img-info">
+                                  APARATE<br>FOTOGRAFIKE
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\smartphones.jpg" >
+                                <div class="img-info">
+                                  SMARTPHONES
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\gaming.jpg" >
+                                <div class="img-info">
+                                   GAMING
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\tv.jpg" >
+                                <div class="img-info">
+                                  TV
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="four">
+                        <div class="four-img">
+                            <div class="img-square">
+                                <img src="photos\pajisje_smart.jpg" class="" >
+                                <div class="img-info">
+                                  PAJISJE<br>SMART
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\network_devices.jpg" >
+                                <div class="img-info">
+                                  PAJISJE PER<br>RRJETA
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\audio.jpg" >
+                                <div class="img-info">
+                                AUDIO
+                                </div>
+                            </div>
+                            <div class="img-square border">
+                                <img src="photos\oferta.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+            </div>  
+              
+            </main>
+
+
+
+            <footer>
+              <?php
+              include_once "C:\xampp\htdocs\Webfaqja\includes\footer.php";
+              ?>
+              </footer>
+            <div class="scroll-bar" ></div>
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+
+<header>
+            <section>
+                
+        <div id="container">
+            
+            <div id="shopName"><a href=""> <b>Tech</b>Ecom </a></div>
+                
+                <div id="search">
+                <input type="text" name="searchBox" placeholder="Search">
+                <input type="button" value="Search">
+            </div>
+            <div id="user">
+                <a href=""> <i class="fas fa-shopping-cart addedToCart"><div id="badge"> 0 </div></i></a>
+    
+            </div>
+    
+            <div class="navbar">
+                <input type="checkbox" name="" id="btn-menu" class="hidden">
+                    <label for="btn-menu" class="icon-menu">
+                <i class="fa fa-bars bars"></i>
+                
+                </label>
+                <div class="navbar_div">
+                    
+                    <ul class="navbar_div_ul">
+                        
+                      <li class="navbar_div_li"><a href="Mainpage.html">Ballina</a></li>
+                      <li class="navbar_div_li"><a href="Produktet.html">Produktet</a></li>
+                      <li class="navbar_div_li"><a href="Rreth Nesh.html">RrethNesh</a></li>
+                      <li class="navbar_div_li"><a href="Kontakt.html">Kontakti</a></li>
+                      <li class="navbar_div_li"><a href="Login.html">Login</a></li>
+                      <li class="navbar_div_li"><a href="Register.html">Register</a></li>
+                      <li class="navbar_div_li"><a href="Shopping.html">Shopping</a></li>
+                    </ul>     
+            </div>
+        </div>
+            </div>
+    
+        </section>
+    </header>
+        <hr>
+</html>
+    </body>
