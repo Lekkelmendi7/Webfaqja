720c9a8ccd42f482414b10d5e208af5c72959dea
Lekkelmendi7 <95182111+Lekkelmendi7@users.noreply.github.com>
2022-04-24 01:38:44 +0200

    Add files via upload

--- Mainpage.php
+++ Mainpage.php
@@ -1,7 +1,20 @@
+
+<?php
+include_once 'classes/product.class.php';
+include_once 'classes/database.class.php';
+
+
+$productClass = new Product();
+$productClass->fetchProducts();
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Faqja Kryesore</title>
         <link rel="stylesheet" type="text/css" href="mainpage.css">
 
@@ -11,16 +24,14 @@
        
     </head>
     <body>
-    <header>
-        <?php include_once 'include/header.php';?>   
-    </header>
+    <?php include_once 'include/header.php';?>
         <hr>
 
         
         <main>
             <div class="main">
               <div class="banner">
-                <img src="photos\comp.jpg" style=" height: 250pt; width:800pt; padding:100pt;"><a href="Mainpage.html"></a>
+                <img src="photos\comp.jpg"><a href="Mainpage.html"></a>
                 <div class="first_txt">Miresevini ne webfaqen tone!</div>
               </div>
               <div class="pic-box">
@@ -31,23 +42,23 @@
                     <div class="four">
                         <div class="four-img">
                             <div class="img-square">
-                                <img src="photos\kompjuter.jpg" class="" style="width:200pt; height:200pt;"><a href="#"></a>
+                                <img src="photos\kompjuter.jpg" class="" ><a href="#"></a>
                                 <div class="img-info">KOMPJUTERE</div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\laptops.jpg" style="width:200pt; height:200pt;"><a href="#"></a>
+                                <img src="photos\laptops.jpg" ><a href="#"></a>
                                 <div class="img-info">
                                   LAPTOP
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\pjese_harduerike.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\pjese_harduerike.jpg" >
                                 <div class="img-info">
                                   PJESE<br>HARDUERIKE
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\aksesore.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\aksesore.jpg" >
                                 <div class="img-info">
                                   AKSESORE
                                 </div>
@@ -57,25 +68,25 @@
                     <div class="four">
                         <div class="four-img">
                             <div class="img-square">
-                                <img src="photos\aparate_fotografike.jpg" class="" style="width:200pt; height:200pt;">
+                                <img src="photos\aparate_fotografike.jpg" class="" >
                                 <div class="img-info">
                                   APARATE<br>FOTOGRAFIKE
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\smartphones.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\smartphones.jpg" >
                                 <div class="img-info">
                                   SMARTPHONES
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\gaming.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\gaming.jpg" >
                                 <div class="img-info">
                                    GAMING
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\tv.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\tv.jpg" >
                                 <div class="img-info">
                                   TV
                                 </div>
@@ -86,25 +97,25 @@
                     <div class="four">
                         <div class="four-img">
                             <div class="img-square">
-                                <img src="photos\pajisje_smart.jpg" class="" style="width:200pt; height:200pt;">
+                                <img src="photos\pajisje_smart.jpg" class="" >
                                 <div class="img-info">
                                   PAJISJE<br>SMART
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\network_devices.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\network_devices.jpg" >
                                 <div class="img-info">
                                   PAJISJE PER<br>RRJETA
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\audio.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\audio.jpg" >
                                 <div class="img-info">
                                 AUDIO
                                 </div>
                             </div>
                             <div class="img-square border">
-                                <img src="photos\oferta.jpg" style="width:200pt; height:200pt;">
+                                <img src="photos\oferta.jpg">
                             </div>
                         </div>
                     </div>
@@ -117,10 +128,9 @@
             </main>
 
 
-
-            <footer>
+    <footer>
             <?php include_once 'include/footer.php';?>
-              </footer>
-              <script src="https://kit.fontawesome.com/3b1ca000cb.js"></script>
+</footer>
+            <div class="scroll-bar" ></div>
     </body>
 </html>
